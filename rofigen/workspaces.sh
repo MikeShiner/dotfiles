51513e35dc5c13904c7ace60bc04e4d5549f5028
Mike Shiner <michael.shiner@siemens.com>
2020-01-31 16:20:42 +0000

    Add rofigen

---
+++ rofigen/workspaces.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+title="Custom Scripts"
+widthpercent=13
+
+typeset -A menu
+menu=(
+    ["1 Code - Strategy Manager UI"]="code /home/mike/Documents/WkSpaces/stratman-ui.code-workspace"
+    ["2 Code - RTO Architecture"]="code /home/mike/Documents/WkSpaces/rto-infastructure.code-workspace"
+    ["3 Code - Polybar Config"]="code /home/mike/Documents/WkSpaces/polybar.code-workspace"
+    ["4 Code - ITS Angular Theme"]="code /home/mike/Documents/WkSpaces/its-angular.code-workspace"
+)
